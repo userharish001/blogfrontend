8f1eb1ed52bb1df73af1e50762442bd8fb4fc2a1
HarishSharma001 <harish9499428306@gmail.com>
2024-11-06 21:57:28 +0530

    new

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -1,37 +1,43 @@
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import blogimage2 from "/blogimage2.jpg";
-import { Link} from "react-router-dom";
-import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 function Navbars() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => setExpanded(!expanded);
+
+  const handleClose = () => setExpanded(false);
+
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary fixed-top" expanded={expanded}>
         <Container fluid>
-          <img className="fluid nav-image " src={blogimage2} />
-          <Navbar.Toggle aria-controls="navbarScroll" />
+          <img className="fluid nav-image" src={blogimage2} alt="Logo" />
+          <Navbar.Toggle aria-controls="navbarScroll" onClick={handleToggle} />
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0 group-item"
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Link className="items" to="/">
+              <Link className="items" to="/" onClick={handleClose}>
                 Home
               </Link>
-              <Link className="items" to="/posts">
+              <Link className="items" to="/posts" onClick={handleClose}>
                 Posts
               </Link>
-              <Link className="items" to="/about">
+              <Link className="items" to="/about" onClick={handleClose}>
                 About
               </Link>
-              <Link className="items" to="/contact">
+              <Link className="items" to="/contact" onClick={handleClose}>
                 Contactus
               </Link>
-              <Link className="items" to="/signup">
+              <Link className="items" to="/signup" onClick={handleClose}>
                 Dashboard
               </Link>
             </Nav>
@@ -40,9 +46,8 @@ function Navbars() {
               className="items"
               id="navbarScrollingDropdown"
             >
-              <NavDropdown.Item href="/signup">Sign-up</NavDropdown.Item>
-              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-              <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+              <NavDropdown.Item href="/signup" onClick={handleClose}>Sign-up</NavDropdown.Item>
+              <NavDropdown.Item href="/login" onClick={handleClose}>Login</NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
           </Navbar.Collapse>
