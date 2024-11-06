baff63b00964e8171e572d71c53ba2589c27b15d
Harish <harish9499428306@gmail.com>
2024-11-07 00:26:47 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -22,7 +22,7 @@ function Signup() {
     event.preventDefault();
     // console.log(inputs);
     axios
-      .post("https://blogbackend-tbku.onrender.com/register", { name, email, password })
+      .post("https://blogbackend-hzdo.onrender.com/register", { name, email, password })
       .then((res) => {
         console.log(res);
         toast.success("User created successfully!",{position:"top-right"});
